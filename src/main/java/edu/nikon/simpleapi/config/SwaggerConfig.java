640467ca4ac84fed4bc89b7a9a82ef106e994477
Nikon Petrunin <p.nikwv@gmail.com>
2018-07-09 13:24:16 +0400

    Upgrade Swagger to v2.8

--- src/main/java/edu/nikon/simpleapi/config/SwaggerConfig.java
+++ src/main/java/edu/nikon/simpleapi/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,7 +18,10 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("Simple REST API")
                 .description("Simple REST API")
-                .contact("Nikon Petrunin (https://github.com/nikon-petr/simple-api)")
+                .contact(new Contact(
+                        "Nikon Petrunin",
+                        "https://github.com/nikon-petr/simple-api",
+                        "p.nikwv@gmail.com"))
                 .version("1.0")
                 .build();
     }
