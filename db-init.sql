fc3df9aa1b6a311aa12f0c38d2698444edc88f1f
Nikon Petrunin <p.nikwv@gmail.com>
2018-12-12 01:52:03 +0400

    Add docker compose file and cors mapper

---
+++ db-init.sql
@@ -0,0 +1,2 @@
+create user simpleapi_app with password 'q1w2e3';
+create database simpleapi_app_db owner simpleapi_app;
--- src/main/java/edu/nikon/simpleapi/config/WebMvcConfig.java
+++ src/main/java/edu/nikon/simpleapi/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package edu.nikon.simpleapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("PUT", "DELETE", "POST", "GET")
+                .maxAge(3600);
     }
 }
